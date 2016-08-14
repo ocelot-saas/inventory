1af61f1170d04936165f7a90010b913d63089037
Horia Coman <horia141@gmail.com>
2016-08-14 22:45:06 +0300

    Start to add some schema.

---
+++ migrations/0002.create_org.py
@@ -0,0 +1,31 @@
+"""Create the org and the org_users table."""
+
+from yoyo import step
+
+
+__depends__ = ['0001.create_schema']
+
+
+step("""
+CREATE TABLE inventory.org (
+    id SERIAL,
+    time_created TIMESTAMP NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE inventory.org_user (
+    org_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    time_created TIMESTAMP NOT NULL,
+    PRIMARY KEY (org_id, user_id),
+    CONSTRAINT org_users_fk_org_id
+        FOREIGN KEY (org_id) REFERENCES inventory.org(id),
+    CONSTRAINT org_users_uk_org_id
+        UNIQUE (org_id),
+    CONSTRAINT org_users_uk_user_id
+        UNIQUE (user_id)
+);
+""", """
+DROP TABLE IF EXISTS inventory.org_user;
+DROP TABLE IF EXISTS inventory.org;
+""")
---
+++ migrations/0003.create_restaurant.py
@@ -0,0 +1,27 @@
+"""Create the restaurant table."""
+
+from yoyo import step
+
+
+__depends__ = ['0002.create_org']
+
+
+step("""
+CREATE TABLE inventory.restaurant (
+    id SERIAL,
+    org_id INTEGER NOT NULL,
+    time_created TIMESTAMP NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    keywords VARCHAR(100) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    opening_hours JSON NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT restaurant_fk_org_id
+        FOREIGN KEY (org_id) REFERENCES inventory.org(id),
+    CONSTRAINT restaurant_uk_org_id
+        UNIQUE (org_id)
+);
+""", """
+DROP TABLE IF EXISTS identity.restaurant;
+""");
